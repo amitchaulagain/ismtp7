b2395d7a7f499ad3c2978b66624766eea23683c5
amitchaulagain <achaulagain123@gmail.com>
2021-05-04 09:29:46 +0545

    create and edit method in person

--- src/com/company/Chat.java
+++ src/com/company/Chat.java
@@ -2,4 +2,8 @@ package com.company;
 
 public class Chat {
     int num =1;
+
+    public static void main(String[] args) {
+        System.out.println("chat feature");
+    }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -5,21 +5,59 @@ package com.company;
 /*
 * Java support multi-level inheritance but not multiple inheritance
 *
+*
+*  git branch crud-feature
+* git checkout crud-feature
+
+ *
 * */
 public class Person extends Mammal {
-    private String firstName;
-    private String lastName;
+    private String fName;
+    private String lName;
     private int age;
 
+    public String getfName() {
+        return fName;
+    }
 
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
 
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     Person(){
         super("apppppp");
     }
 
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
 
 
+    public void addPerson(Person p){
+        System.out.println("adding person");
+
+    }
+
+    public void edit(Person p){
+        System.out.println("adding person");
+
+    }
 
 
 
