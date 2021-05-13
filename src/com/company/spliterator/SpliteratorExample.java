b93bb27bddda33581505ae7655e501c38ec7c785
Sandip Shiwakoti <62270918+sandipshiwakoti@users.noreply.github.com>
2021-05-13 10:07:37 +0545

    Add spliterator package

---
+++ src/com/company/spliterator/SpliteratorExample.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class SpliteratorExample {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        Spliterator<Integer> sit = list.spliterator();
+
+        System.out.println("Number of elements need to be iterated: " + sit.estimateSize());
+
+        Spliterator<Integer> sit1 = sit.trySplit();
+
+
+        System.out.println("Number of elements need to be iterated: " + sit.estimateSize());
+        System.out.println("Number of elements need to be iterated: " + sit1.estimateSize());
+
+        System.out.println("First half elements: ");
+        while(sit1.tryAdvance(System.out::println)){};
+        // sit1.forEachRemaining(System.out::println);
+
+        System.out.println("Last half elements: ");
+        while(sit.tryAdvance(System.out::println));
+        // sit1.forEachRemaining(System.out::println);
+
+        System.out.println("Number of elements need to be iterated: " + sit.estimateSize());
+        System.out.println("Number of elements need to be iterated: " + sit1.estimateSize());
+    }
+}
